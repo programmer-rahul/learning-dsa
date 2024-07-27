a410084a2094ea87bb8bb11fb2dc93ad725123fa
programmer-rahul <7678478061k@gmail.com>
2024-07-27 15:16:30 +0530

    insertion sort

--- important_sorting_techniques/sorting_1/main.cpp
+++ important_sorting_techniques/sorting_1/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void selection_sort(int n, int arr[]);
 void bubble_sort(int n, int arr[]);
+void insertion_sort(int n, int arr[]);
 
 int main()
 {
@@ -12,7 +13,8 @@ int main()
     int arr[n] = {13, 46, 24, 52, 20, 9};
 
     // selection_sort(n, arr);
-    bubble_sort(n, arr);
+    // bubble_sort(n, arr);
+    insertion_sort(n, arr);
 
     for (int i : arr)
     {
@@ -73,6 +75,52 @@ void bubble_sort(int n, int arr[])
         if (is_swapped == 0)
         {
             break;
+        }
+    }
+}
+
+void insertion_sort(int n, int arr[])
+{
+
+    // int arr[n] = {13, 46, 24, 52, 20, 9};
+
+    /*
+        i = 0 {13,46} {13,46,24,52,20,9}
+        i = 1 {13,46,24} {13,24,46,52,20,9}
+        i = 2 {13,24,46,52} {13,24,46,52,20,9}
+        i = 3 {13,24,46,52,20} {13,20,24,46,52,9}
+        i = 4 {13,20,24,46,52,9} {9,13,20,24,46,52}
+
+
+        n = 6;
+
+        outerLoop = n - 1;
+        innerLoop = i + 1;
+    */
+
+    // {13, 46, 24, 15, 20, 9};
+    /*
+        i = 1;
+        j = 1;
+
+        i = 1 = {13,46}
+        i = 2 = {13,24,46}
+        i = 3 = {13,24,15,46}
+    */
+
+    for (int i = 1; i < n; i++)
+    {
+        int current_index = i;
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j - 1] > arr[current_index])
+            {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[current_index];
+                arr[current_index] = temp;
+
+                current_index--;
+            }
         }
     }
 }
